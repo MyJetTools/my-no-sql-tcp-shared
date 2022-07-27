0bf1bcd976a9f29a57b9e8fc61341ec6ddd92515
amigin <amigin@gmail.com>
2022-07-27 18:03:02 +0300

    Added PayloadCompressors

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod common_deserializers;
 pub mod common_serializers;
 mod delete_row_tcp_contract;
+pub mod payload_comressor;
 mod tcp_contracts;
 pub mod tcp_packets;
 mod tcp_serializer;
 pub use delete_row_tcp_contract::DeleteRowTcpContract;
 pub use tcp_contracts::TcpContract;
 pub use tcp_serializer::MyNoSqlReaderTcpSerializer;
+mod vec_writer;
---
+++ src/payload_comressor.rs
@@ -0,0 +1,53 @@
+use crate::vec_writer::VecWriter;
+use std::io::{Cursor, Read, Write};
+
+pub fn compress(payload: Vec<u8>) -> Result<Vec<u8>, zip::result::ZipError> {
+    let mut writer = VecWriter::new();
+
+    {
+        let mut zip = zip::ZipWriter::new(&mut writer);
+
+        let options =
+            zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
+
+        zip.start_file("d", options)?;
+
+        let mut pos = 0;
+        while pos < payload.len() {
+            let size = zip.write(&payload[pos..])?;
+
+            pos += size;
+        }
+
+        zip.finish()?;
+    }
+
+    Ok(writer.buf)
+}
+
+pub fn decompress(payload: Vec<u8>) -> Result<Vec<u8>, zip::result::ZipError> {
+    let c = Cursor::new(payload.to_vec());
+
+    let mut zip = zip::ZipArchive::new(c)?;
+
+    let mut page_buffer: Vec<u8> = Vec::new();
+
+    for i in 0..zip.len() {
+        let mut zip_file = zip.by_index(i)?;
+
+        if zip_file.name() == "d" {
+            let mut buffer = [0u8; 1024 * 1024];
+
+            loop {
+                let read_size = zip_file.read(&mut buffer[..])?;
+                if read_size == 0 {
+                    break;
+                }
+
+                page_buffer.extend(&buffer[..read_size]);
+            }
+        }
+    }
+
+    Ok(page_buffer)
+}
